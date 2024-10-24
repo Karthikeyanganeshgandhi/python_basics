feeeb33d9e2c08b7d9077454ecca85f369d86ce7
Karthikeyanganeshgandhi <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-10-24 13:00:03 +0530

    Add files via upload

---
+++ decisionmaking.py
@@ -0,0 +1,48 @@
+# if statement
+# a=40
+# b=200
+# if b>a:
+#     print("a is greater than b")
+
+# a=400
+# b=700
+# if b>a:
+#     print("b is greater than a")
+
+# elif 
+# a=60
+# b=60
+# if a>b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a is equal to b")
+
+# a=80
+# b=90
+# if a > b:
+#     print("a is greater than b")
+# elif a == b:
+#     print("a is equal to b")
+# if b<a:
+#     print("b is less than a")
+# elif a!=b:
+#     print("a is not equal to b")
+
+# else statement
+# a=800
+# b=100
+# if b>a:
+#     print("b is greater than a")
+# elif a<b:
+#     print("a is less than b")
+# else:
+#     print("a is greater than b")
+
+# nested if statement
+mark=80
+if mark>=80:
+    print("pass")
+if mark<=75:
+    print("just pass")
+else:
+    print("failed")
---
+++ tsk.py
@@ -0,0 +1,14 @@
+# a=int(input("enter your mark"))
+# b=25
+# if a<b:
+#     print("congrats you are failed")
+# elif a == b:
+#     print("congrats you are just passed")
+# else:
+#     print("u r qualified")
+
+a=int(input("enter your number"))
+if a%1:
+    print("this ia an odd number")
+else:
+    print("this is an even number")
