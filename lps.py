3e120ec7cd18c5ca58fe86743e36f47da60ad2a1
Karthikeyanganeshgandhi <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-10-28 09:42:00 +0530

    Add files via upload

---
+++ lps.py
@@ -0,0 +1,104 @@
+# loops
+# for loop
+# fruits=["pineapple","orange","grape","apple"]
+# for x in fruits:
+#     print(x)
+
+# stdnts=["aswin","akash","karthik"]
+# for x in stdnts:
+#     print(x)
+
+# looping through a string
+# stdnts=["aswin","akash","karthik"]
+# for x in "aswin":
+#     print(x)
+
+# foods=["biriryani","chappathi","dosa"]
+# for x in "dosa":
+#     print(x)
+
+# while loop
+# a=0
+# while a<11:
+#     print(a)
+#     a+=1
+
+# control statements-two types-break&continue
+
+# break
+# i=1
+# while i<20:
+#     print(i)
+#     if i==10:
+#         break
+#     i+=1
+
+# z=1
+# while z<30:
+#     print(z)
+#     if z==25:
+#         break
+#     z+=1
+
+# c=2
+# while c<500:
+#     c+=1
+#     if c==100:
+#         continue
+#     print(c)
+#     if c==300:
+#         break
+
+# else statement
+# a=1
+# while a<7:
+#     print(a)
+#     a+=1
+# else:
+#     print("a is no longer less than 7")
+
+# z=10
+# while z<50:
+#     print(z)
+#     z+=1
+# else:
+#     print("odippoda 50 onnum ivde illa")
+
+# break in for loop
+
+# bike=["kawasaki","bmw","pulsar","ktm"]
+# for x in bike:
+#     print(x)
+#     if x=="bmw":
+#         break
+
+# objects=["mirror","pot","lamp","chair"]
+# for x in objects:
+#     print(x)
+#     if x=="lamp":
+#         break
+
+# objects=["mirror","pot","lamp","chair"]
+# for x in objects:
+#     if x=="lamp":
+#         continue
+#     print(x)
+
+# range in for loop
+# for x in range(1,50,2):
+#     print(x)
+
+# for x in range(2,30,3):
+#     print(x)
+
+# pass statement
+
+# for x in (1,40,4):
+    # pass
+
+for x in (1,40,2):
+    pass
+    
+    
+    
+     
