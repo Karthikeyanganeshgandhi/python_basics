b77b12937648c8cb617124f9ed0f057fb0973a20
Karthikeyanganeshgandhi <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-10-29 13:14:54 +0530

    Add files via upload

---
+++ lst.py
@@ -0,0 +1,14 @@
+# thislist=["apple","banana","orange"]
+# print(thislist)
+
+# thislist=["apple","banana","orange"]
+# print(len(thislist))
+
+# thislist=["apple","banana","orange"]
+# thislist1=[1,2,3]
+# thislist2=[True,False,True]
+# print(thislist)
+# print(thislist1)
+# print(thislist2)
+
+# thislist=["abc",40,True,]
