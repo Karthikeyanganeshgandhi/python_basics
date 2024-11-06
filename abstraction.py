35497f9525582419d92d63cab003206c87d8a361
Karthikeyanganeshgandhi <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-11-06 09:39:11 +0530

    Add files via upload

---
+++ abstraction.py
@@ -0,0 +1,29 @@
+# abstraction
+# hiding of irrelevant data and displaying of relevant data is known as abstraction
+
+# from abc import ABC,abstractmethod
+# class car(ABC):
+#     def mileage(self):
+#         pass
+# class maruthi(car):
+#     def mileage(self):
+#         return "the mileage is 25 mph"
+# class renault(car):
+#     def mileage(self):
+#         return "the mileage is 19 mph"
+# class bmw(car):
+#     def mileage(self):
+#         return "the mileage is 10 mph"
+# m=maruthi()
+# print(m.mileage())
+# r=renault()
+# print(r.mileage())
+# b=bmw()
+# print(b.mileage())
+
+
+
+
+  
+
+    
