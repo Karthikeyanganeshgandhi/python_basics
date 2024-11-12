427e8e94d22d2fa1d04fe59907011a4427df11ae
Karthikeyanganeshgandhi <91650724+Karthikeyanganeshgandhi@users.noreply.github.com>
2024-11-12 19:44:26 +0530

    Add files via upload

---
+++ file handling.py
@@ -0,0 +1,46 @@
+# file=open("dummy.txt","x")
+
+# writing
+
+# file=open("dummy.txt","w")
+# file.write("hello! welcome to my world")
+# file.close()
+
+# reading a file
+
+# file=open("dummy.txt","r")
+# print(file.read())
+
+# append
+
+# file=open("dummy.txt","a")
+# file.write(", brace yourself before we going to explore my world")
+# file.close()
+
+# file=open("dummy.txt","r")
+# print(file.read())
+
+# file=open("dummy.txt","r")
+# print(file.read(5))
+
+# file=open("dummy.txt","r")
+# print(file.readline())
+
+# file=open("dummy.txt","r")
+# print(file.readlines())
+
+# write lines
+
+# file=open("dummy.txt","w")
+# lines=["this is a line /n","this is a line /n","this is a line /n"]
+# file.writelines(lines)
+# file.close()
+
+# delete a file
+
+# import os
+# os.remove("dummy.txt")
+
+# with open("dummy.txt", "r") as file1:
+#     read_content = file1.read()
+#     print(read_content)
